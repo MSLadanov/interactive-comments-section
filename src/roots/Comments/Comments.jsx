504df601228cc8dc873735abe7f5219445d29020
Михаил Ладанов <MSLadanov@gmail.com>
2023-12-13 19:12:20 +0300

    fetch data

--- src/roots/Comments/Comments.jsx
+++ src/roots/Comments/Comments.jsx
@@ -1,6 +1,11 @@
+import axios from 'axios'
 import React from 'react'
 
 export default function Comments() {
+  function getComments(){
+    axios.get('/data.json').then((res) => console.log(res.data)).catch((err) => console.log(err))
+  }
+  getComments()
   return (
     <div>Comments</div>
   )
--- src/roots/Profile/Profile.jsx
+++ src/roots/Profile/Profile.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function Profile() {
+  let { profileId } = useParams();
+  console.log(profileId)
   return (
     <div>Profile</div>
   )
