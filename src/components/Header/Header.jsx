4c267b1d97d5fdfac925ccaf8ce015bfb5088c45
Михаил Ладанов <MSLadanov@gmail.com>
2023-12-15 19:28:06 +0300

    navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,10 @@ import './style.css'
 export default function Header() {
   return (
     <div className='header'>
-      <div className='logo'></div>
+      <div className='logo'>
+        <img src="/images/logo.svg" alt="logo"/>
+        <h1>Comments</h1>
+      </div>
       <div className='menu'></div>
     </div>
   )
