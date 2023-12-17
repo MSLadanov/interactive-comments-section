1d2504cf3a631b4f55e8ee7c826f4f88e3b788d1
Михаил Ладанов <MSLadanov@gmail.com>
2023-12-17 17:49:39 +0300

    comments

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -4,6 +4,33 @@ import './style.css'
 export default function Comment({comment, setComments}) {
   console.log(comment)
   return (
-    <div>Comment</div>
+    <div className='comment'>
+      <div className='vote-section'>
+        <div className='comment-vote'>
+          <div className="comment-like">
+            <img src="/images/icon-plus.svg" alt="like" />
+          </div>
+          <div className="comment-likes">{comment.score}</div>
+          <div className="comment-dislikes">
+            <img src="/images/icon-minus.svg" alt="dislike" />
+          </div>
+        </div>
+      </div>
+      <div className='comment-section'>
+        <div className="comment-header">
+          <div className='comment-info'>
+            <img src={`/images/avatars/image-${comment.user.username}.png`} alt="user" />
+            <h4>{comment.user.username}</h4>
+            <h5>{comment.createdAt}</h5>
+          </div>
+          <div className='comment-reply'>
+
+          </div>
+        </div>
+        <div className='comment-text'>
+          {comment.content}
+        </div>
+      </div>
+    </div>
   )
 }
