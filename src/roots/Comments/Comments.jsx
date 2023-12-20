b70ec3f131f9b3c3c7fb230ca6ea1f395c5f096f
Михаил Ладанов <MSLadanov@gmail.com>
2023-12-20 19:45:50 +0300

    comments

--- src/roots/Comments/Comments.jsx
+++ src/roots/Comments/Comments.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { useOutletContext } from 'react-router-dom'
 import Comment from '../../components/Comment/Comment'
+import './style.css'
 
 export default function Comments() {
   const [comments, setComments, userData, setUserData] = useOutletContext()
   if(comments){return (
-    <div>{comments.map((comment) => <Comment key={comment.id} comment={comment} setComments={setComments}/>)}</div>
+    <div className='comments'>{comments.map((comment) => <Comment key={comment.id} comment={comment} setComments={setComments}/>)}</div>
   )}
 }
