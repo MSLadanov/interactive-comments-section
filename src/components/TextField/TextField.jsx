44a88e320e2eb4e9edfde3ff354c43a608f1e491
Михаил Ладанов <MSLadanov@gmail.com>
2023-12-25 19:14:28 +0300

    comments

---
+++ src/components/TextField/TextField.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './style.css'
+
+export default function TextField({userData}) {
+  return (
+    <div className='text-field'>
+        <div className="comment">
+            <div className="comment-info">
+                <img src={`/images/avatars/image-${userData.username}.png`} alt="user" />
+            </div>
+            <div className="text-field-input">
+                <textarea name="" id="" cols="30" rows="10"></textarea>
+            </div>
+            <div className="send-comment-btn">
+                <button type="button">SEND</button>
+            </div>
+        </div>
+    </div>
+  )
+}
--- src/roots/Comments/Comments.jsx
+++ src/roots/Comments/Comments.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import { useOutletContext } from 'react-router-dom'
 import Comment from '../../components/Comment/Comment'
 import './style.css'
+import TextField from '../../components/TextField/TextField'
 
 export default function Comments() {
   const [comments, setComments, userData, setUserData] = useOutletContext()
-  if(comments){return (
+  if(comments){
+    return (
+    <>
     <div className='comments'>{comments.map((comment) => <Comment key={comment.id} comment={comment} setComments={setComments} userData={userData} setUserData={setUserData}/>)}</div>
+    <TextField userData={userData}/>
+    </>
   )}
 }
