408a8c53dc6980f331ab29cdb67d2e57a66a5f0a
Михаил Ладанов <MSLadanov@gmail.com>
2024-01-13 13:44:17 +0300

    comments

---
+++ api/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def hello_world():
+    return {
+        'message': 'Hello test API!'
+    }
