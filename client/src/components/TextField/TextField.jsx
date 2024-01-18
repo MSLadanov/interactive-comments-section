bd485aca06b76907eb84ecd686e906247475e184
Михаил Ладанов <MSLadanov@gmail.com>
2024-01-18 19:56:29 +0300

    comments

--- client/src/components/TextField/TextField.jsx
+++ client/src/components/TextField/TextField.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useState } from 'react'
 import './style.css'
 
 export default function TextField({userData}) {
+  const [commentText, setCommentText] = useState('')
+  function addComment(){
+    console.log('add comment')
+    console.log(commentText)
+    setCommentText('')
+  }
   return (
     <div className='text-field'>
         <div className="comment">
@@ -9,10 +16,10 @@ export default function TextField({userData}) {
                 <img src={`/images/avatars/image-${userData.username}.png`} alt="user" />
             </div>
             <div className="text-field-input">
-                <textarea name="" id="" cols="30" rows="5" placeholder='Add a comment...'></textarea>
+                <textarea name="" id="" cols="30" rows="5" placeholder='Add a comment...' onChange={(e) => setCommentText(e.target.value) }></textarea>
             </div>
             <div className="send-comment-btn">
-                <button type="button">SEND</button>
+                <button onClick={() => addComment()} type="button">SEND</button>
             </div>
         </div>
     </div>
